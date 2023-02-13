daf101dcad648537ada37593cc923a55ed994f97
Patrik Šoma <soma.patrik@gmail.com>
2023-02-13 22:37:20 +0100

    večer

--- Veverka/ViewModels/MainViewModel.cs
+++ Veverka/ViewModels/MainViewModel.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Veverka.Views;
 
 namespace Veverka.ViewModels
 {
     public class MainViewModel : PrimeViewModel
     {
+        public ICommand CreatePlc { private set; get; }
 
+        public MainViewModel()
+        {
+            CreatePlc = new Command(CreatePlcHandler);
+        }
+
+        private async void CreatePlcHandler()
+        {
+            EditS7PlcPage edit = new EditS7PlcPage();
+            await Shell.Current.Navigation.PushAsync(edit);
+        }
     }
 }
